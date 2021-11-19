0375b02a57dcac8bf34acd401e466dbe3c450706
MarinaSev <pawpawsap@gmail.com>
2021-11-19 21:25:28 +0300

    initial commit

---
+++ src/test/java/MobileBankApiTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MobileBankApiTest {
+
+    @Test
+    public void shouldCheck() {
+        // Given - When - Then
+        // Предусловия
+        given()
+           .baseUri("https://postman-echo.com")
+           .body("some data")
+        // Выполняемые действия
+           .when()
+                .post("/post")
+        // ПроверкиL
+           .then()
+                .statusCode(200)
+           .body("data", equalTo("some data"))
+        ;
+    }
+}
